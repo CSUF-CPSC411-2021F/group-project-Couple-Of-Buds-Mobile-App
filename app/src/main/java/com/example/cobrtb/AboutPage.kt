0b62fc319e1846b36256879ed8628b9f70e9605e
henryng310 <hnguyen310@csu.fullerton.edu>
2021-09-22 16:15:38 -0700

    Created draft for About_Page class

--- app/src/main/java/com/example/cobrtb/AboutPage.kt
+++ app/src/main/java/com/example/cobrtb/AboutPage.kt
@@ -1,5 +1,22 @@
 package com.example.cobrtb
 
-class AboutPage {
-    //code here
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    var hostInfo: List<String> = mutableListOf("Lyba", "Selwa")
+
+    fun displayHosts(){
+        //display the hosts name and info
+    }
+
 }
+
+
+
