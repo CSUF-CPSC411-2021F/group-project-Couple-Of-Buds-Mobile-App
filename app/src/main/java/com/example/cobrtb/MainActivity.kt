07501863b20d5dac52e2e2d1ace2966b8e4fc7f7
William Pham Duong <wpduong@csu.fullerton.edu>
2021-09-21 18:50:39 -0700

    Project Creation

---
+++ app/src/main/java/com/example/cobrtb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cobrtb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
