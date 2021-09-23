f72711fb8a990b1b2a43bc5ac3c3acf873b79d53
henryng310 <hnguyen310@csu.fullerton.edu>
2021-09-22 20:13:31 -0700

    Created draft for About_Page class

--- app/src/main/java/com/example/cobrtb/AboutPage.kt
+++ app/src/main/java/com/example/cobrtb/AboutPage.kt
@@ -1,22 +1,24 @@
 package com.example.cobrtb
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+
 
 class AboutPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
     }
+}
+
+class About(var hosts: String, var brand: String, var devs: String){
 
     var hostInfo: List<String> = mutableListOf("Lyba", "Selwa")
 
-    fun displayHosts(){
+    fun displayHosts() {
         //display the hosts name and info
+        //use xml to display pictures, etc
     }
-
 }
-
-
-
