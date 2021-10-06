2d6cb82c6abe6861e72b8a8acb092be83564d297
lybabatla <lybabatla@csu.fullerton.edu>
2021-10-05 19:32:22 -0700

    Promo_Page Second Check-In

---
+++ app/src/main/java/com/example/cobrtb/Promo_profile.kt
@@ -0,0 +1,11 @@
+package com.example.cobrtb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Promo_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promo_profile)
+    }
+}
