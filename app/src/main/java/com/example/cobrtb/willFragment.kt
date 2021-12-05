d61542286907764eeb338b9778be47b719a8f278
tRamsay93 <54593330+tRamsay93@users.noreply.github.com>
2021-12-04 16:55:07 -0800

    Almost Done

--- app/src/main/java/com/example/cobrtb/willFragment.kt
+++ app/src/main/java/com/example/cobrtb/willFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -17,7 +15,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class willFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,10 +26,8 @@ class willFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_will, container, false)
     }
@@ -46,14 +41,12 @@ class willFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment willFragment.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            willFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+      fun newInstance(param1: String, param2: String) =
+                willFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
