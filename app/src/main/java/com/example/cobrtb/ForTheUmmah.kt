22c084cf852164fc6adeba0c7e62b146c3defac4
lybabatla <lybabatla@csu.fullerton.edu>
2021-12-05 19:09:13 -0800

    Updated2

--- app/src/main/java/com/example/cobrtb/ForTheUmmah.kt
+++ app/src/main/java/com/example/cobrtb/ForTheUmmah.kt
@@ -2,10 +2,23 @@ package com.example.cobrtb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class ForTheUmmah : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_for_the_ummah)
+
+        webViewSetup()
+    }
+
+    private fun webViewSetup() {
+        val mWebView = findViewById<WebView>(R.id.link2)
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.apply {
+            loadUrl("https://fortheummah.co/")
+        }
     }
 }
