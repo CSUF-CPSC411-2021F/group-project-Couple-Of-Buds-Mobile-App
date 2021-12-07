36374aac28f2149a8c53c1b3503ef0db894b1e3a
lybabatla <lybabatla@csu.fullerton.edu>
2021-12-06 19:04:20 -0800

    Updated3

--- app/src/main/java/com/example/cobrtb/MahnjaanDecorates.kt
+++ app/src/main/java/com/example/cobrtb/MahnjaanDecorates.kt
@@ -1,15 +1,28 @@
 package com.example.cobrtb
 
+import android.widget.MediaController
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.VideoView
+import android.view.View
 
 class MahnjaanDecorates : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mahnjaan_decorates)
-
+        val mediaController = MediaController(this)
+        val mVideoView = findViewById<VideoView>(R.id.videoView)
+        mediaController.setAnchorView(mVideoView)
+        val localUri = Uri.parse("android.resource://$packageName/${R.raw.mahnjaanvid}")
+        mVideoView.setMediaController(mediaController)
+        mVideoView.setVideoURI(localUri)
+        mVideoView.requestFocus()
+        mVideoView.start()
+        
         webViewSetup()
     }
 
